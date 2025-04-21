ce3344dd4dcb2c0e6c4c452a820db1f679d82557
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2025-04-21 18:10:45 +0530

    patch

--- Assignments/Assignment2/codes/2/2_adder.v
+++ Assignments/Assignment2/codes/2/2_adder.v
@@ -1,7 +1,7 @@
-module EightBitAdder(input [7:0] a,input [7:0] b,output [7:0]sum, output cout);
+module EightBitAdder(input [7:0] a,input [7:0] b,input cin,output [7:0]sum, output cout);
 genvar i;
 wire [8:0] c;
-assign c[0]=0;
+assign c[0]=cin;
 generate
 	for(i=0;i<8;i=i+1)begin 
 		bitadder adders(.a(a[i]),.b(b[i]),.sum(sum[i]),.cin(c[i]),.cout(c[i+1]));
--- Assignments/Assignment2/codes/2/2_test.v
+++ Assignments/Assignment2/codes/2/2_test.v
@@ -3,22 +3,23 @@
 module test_8bitadder;
 
 reg [7:0]a,b;
+reg cin;
 wire [7:0] sum;
 wire cout;
 
-EightBitAdder dusra(.a(a),.b(b),.sum(sum),.cout(cout));
+EightBitAdder dusra(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
 
 initial begin
         $dumpfile("8bitadder.vcd");
         $dumpvars(0,test_8bitadder);
 	
-        $display("Time\ta\tb\tsum\tcout");
-        $monitor("%0dns\t%b\t%b\t%b\t%b", $time, a,b, sum,cout);
-        a=8'b00000000;b=8'b00000000;#10;
-	a=8'b11111111;b=8'b11111111;#10;
-	a=8'b10101010;b=8'b11111111;#10;
-	a=8'b10111011;b=8'b10101110;#10;
-	a=8'b11010101;b=8'b10011101;#10;
+        $display("Time\ta\tb\tcin\tsum\tcout");
+        $monitor("%0dns\t%b\t%b\t%b\t%b\t%b", $time, a,b,cin,sum,cout);
+        a=8'b00000000;b=8'b00000000;cin=1'b1;#10;
+	a=8'b11111111;b=8'b11111111;cin=1'b0;#10;
+	a=8'b10101010;b=8'b11111111;cin=1'b1;#10;
+	a=8'b10111011;b=8'b10101110;cin=1'b0;#10;
+	a=8'b11010101;b=8'b10011101;cin=1'b1;#10;
 
 	$finish;
 end
